d34fc3e468c63352ab01fa323bfb810bcc7131e2
Natanael <nata.mw@hotmail.com>
2023-09-15 17:32:26 -0300

    added cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+import { fastifyCors } from '@fastify/cors'
 import fastify from 'fastify'
 import { createTranscriptionRoute } from './routes/create-transcription'
 import { generateAICompletionRoute } from './routes/generate-ai-completion'
@@ -6,6 +7,10 @@ import { getAllVideosRoute } from './routes/get-all-videos'
 import { uploadVideoRoute } from './routes/upload-video'
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*'
+})
 
 app.register(getAlPromptsRoute)
 app.register(uploadVideoRoute)
